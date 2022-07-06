13707e4ac1440ce4130a3031d18dc0ffa43809ac
OshiroSama <rkenig@aol.com>
2022-07-07 00:55:39 +0300

    Week 4 Day 4 Daily completed

--- Week4/Day4/DailyChallenge/script.js
+++ Week4/Day4/DailyChallenge/script.js
@@ -1,9 +1,18 @@
 function bottlesOfBeer(bottles) {
   let bottlesLeft = bottles;
   let takeOff = 1;
-  while (bottles >= 0) {
-    let itThem = "";
-    console.log(`${bottlesLeft} bottles of beer on the wall\n${bottlesLeft}\n`);
+  while (bottlesLeft > 0) {
+    if (bottlesLeft - takeOff <= 0) {
+      takeOff = bottlesLeft;
+    }
+    let itThem = takeOff == 1 ? "it" : "them";
+    console.log(`
+    ${bottlesLeft} bottles of beer on the wall\n
+    ${bottlesLeft} bottles of beer\n
+    Take ${takeOff} down, pass ${itThem} around\n
+    ${bottlesLeft - takeOff} bottles of beer on the wall`);
+    bottlesLeft -= takeOff;
+    takeOff++;
   }
 }
 
