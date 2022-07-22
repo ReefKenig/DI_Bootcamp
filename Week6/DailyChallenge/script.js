4ec7b7a9a951ad5dc6f1eb74364ff430e44d2a2f
OshiroSama <rkenig@aol.com>
2022-07-22 12:09:58 +0300

    Week 6 Day 2 Daily completed

---
+++ Week6/DailyChallenge/script.js
@@ -0,0 +1,35 @@
+const gameInfo = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"],
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"],
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"],
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"],
+  },
+];
+
+let usernames = [];
+let winners = [];
+let totalScore = 0;
+gameInfo.forEach((element) => {
+  usernames.push(element.username + "!");
+  element.score > 5 ? winners.push(element.username) : 0;
+  totalScore += element.score;
+});
