916885f439cc4c61f7e2c58eaae5e65b49f240d4
OshiroSama <rkenig@aol.com>
2022-09-11 13:19:08 +0300

    Week 12 Day 4 XP2 completeed

--- Week12/Day4/ExercisesXP2/Ex2/server.js
+++ Week12/Day4/ExercisesXP2/Ex2/server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+const app = express();
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const idObj = { id: id };
+  console.log(idObj);
+  res.json(idObj);
+});
+app.listen(3000, () => {
+  console.log("running on port 3000");
+});
