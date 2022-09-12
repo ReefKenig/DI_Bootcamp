0b1dfada9f72736ac4c344ea0c2a9e4cca0a1941
OshiroSama <70912976+OshiroSama@users.noreply.github.com>
2022-09-12 12:00:08 +0300

    Week 12 Day 4 daily completed

--- Week12/Day4/DailyChallenge/server.js
+++ Week12/Day4/DailyChallenge/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
+// Display hobby
 app.get("/aboutMe/:hobby", (req, res) => {
   const hobby = req.params.hobby;
   if (!isNaN(hobby) || hobby.length <= 0) {
@@ -10,30 +14,25 @@ app.get("/aboutMe/:hobby", (req, res) => {
   res.send(hobby);
 });
 
+// Show picture
 app.get("/pic", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.writeHead(200);
-  res.end(
-    "<html><body><img src='https://i.ytimg.com/vi/OMx1z3q2a_I/maxresdefault.jpg' alt='pic'></img></body></html>"
-  );
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(
+      "<html><body><img src='https://i.ytimg.com/vi/OMx1z3q2a_I/maxresdefault.jpg' alt='pic'></img></body></html>"
+      );
+    });
+
+// Send form
+app.use("/form", express.static(__dirname + "/public"));
+
+// Redirect to see message
+app.get("/formData", (req, res) => {
+    const response = req.query;
+    res.send(`${response.email} sent you a message "${response.message}"`);
 });
 
-app.get("/form", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.get("/form", (req, res) => {
-  const email = req.body.email;
-  const message = req.body.message;
-  res.end(`${email} sent you a message "${message}"`);
-});
-
-app.post("/formData", (req, res) => {
-  const email = req.body.email;
-  const message = req.body.message;
-  return res.send(`${email} sent you a message "${message}"`);
-});
-
-app.listen(3000, () => {
-  console.log("Listening on 3000");
-});
+// Listen on 5000
+app.listen(5000, () => {
+    console.log("Listening on 5000");
+})
